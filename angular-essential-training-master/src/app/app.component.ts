d43270f70fe24d40d6d27891ee7d5819dc90b8ca
ikikika <iki.jito@gmail.com>
2021-04-29 18:08:35 +0800

    component selector

--- angular-essential-training-master/src/app/app.component.ts
+++ angular-essential-training-master/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component} from '@angular/core'
 
 @Component({
-    selector: 'app-root',
+    selector: 'mw-app', // angular will look for this tag in index.html 
     template: '<h1>My app</h1>'
 })
 export class AppComponent {}
