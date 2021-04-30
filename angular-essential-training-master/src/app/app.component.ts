f4266d10304f578cf3aec1cebb203accbde385ad
ikikika <iki.jito@gmail.com>
2021-04-30 11:44:04 +0800

    styleing from component

--- angular-essential-training-master/src/app/app.component.ts
+++ angular-essential-training-master/src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component} from '@angular/core'
 
 @Component({
     selector: 'mw-app', // angular will look for this tag in index.html 
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styles: [
+        `
+            h1 { color: #fff; }
+            .description { color: yellow; }
+        `,
+        `
+            .description2 { color: green }
+        `
+    ]
 })
 export class AppComponent {}
