0d1911ba09a2f36039d3116267d3abd5eca389ec
ikikika <iki.jito@gmail.com>
2021-04-30 17:43:01 +0800

    render basic component in app

--- angular-essential-training-master/src/app/app.module.ts
+++ angular-essential-training-master/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser'
 import {AppComponent} from './app.component'
 import {MediaItemComponent} from './media-item.component'
+import {MediaItemListComponent} from './media-item-list.component'
 
 @NgModule({
-    imports:[ BrowserModule ], // bring in other angular modules that my module will need
-    declarations: [ AppComponent, MediaItemComponent ], // used to make components, directives and pipes available to your module that dun come from another module
+    imports: [ BrowserModule ], // bring in other angular modules that my module will need
+    declarations: [ 
+        AppComponent, 
+        MediaItemComponent, 
+        MediaItemListComponent 
+    ], // used to make components, directives and pipes available to your module that dun come from another module
     bootstrap: [ AppComponent ] // used for a root module and let Angular know which component or components will be the starting point for bootstrap process // entry point for app code
 })
 export class AppModule {}
