17bbe31a258c788d1f8a5fd78df6d7a44415d8c7
ikikika <iki.jito@gmail.com>
2021-04-30 11:46:30 +0800

    styleing from external file

--- angular-essential-training-master/src/app/app.component.ts
+++ angular-essential-training-master/src/app/app.component.ts
@@ -3,14 +3,6 @@ import { Component} from '@angular/core'
 @Component({
     selector: 'mw-app', // angular will look for this tag in index.html 
     templateUrl: './app.component.html',
-    styles: [
-        `
-            h1 { color: #fff; }
-            .description { color: yellow; }
-        `,
-        `
-            .description2 { color: green }
-        `
-    ]
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
