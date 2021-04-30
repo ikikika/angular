18a221f85104bd9f91c74ae1cecf58e454e9344e
ikikika <iki.jito@gmail.com>
2021-04-30 13:39:37 +0800

    using other component in a component

--- angular-essential-training-master/src/app/app.module.ts
+++ angular-essential-training-master/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser'
 import {AppComponent} from './app.component'
+import {MediaItemComponent} from './media-item.component'
 
 @NgModule({
     imports:[ BrowserModule ], // bring in other angular modules that my module will need
-    declarations: [ AppComponent ], // used to make components, directives and pipes available to your module that dun come from another module
+    declarations: [ AppComponent, MediaItemComponent ], // used to make components, directives and pipes available to your module that dun come from another module
     bootstrap: [ AppComponent ] // used for a root module and let Angular know which component or components will be the starting point for bootstrap process // entry point for app code
 })
 export class AppModule {}
