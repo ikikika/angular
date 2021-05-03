2577b30e4a6c77010b21116176033fd6239aa62f
ikikika <iki.jito@gmail.com>
2021-05-03 13:40:41 +0800

    attribute direcive: custom

--- angular-essential-training-master/src/app/app.module.ts
+++ angular-essential-training-master/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser'
-import {AppComponent} from './app.component'
-import {MediaItemComponent} from './media-item.component'
-import {MediaItemListComponent} from './media-item-list.component'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppComponent } from './app.component'
+import { MediaItemComponent } from './media-item.component'
+import { MediaItemListComponent } from './media-item-list.component'
+import { FavoriteDirective } from './favorite.directive'
 
 @NgModule({
     imports: [ BrowserModule ], // bring in other angular modules that my module will need
     declarations: [ 
         AppComponent, 
         MediaItemComponent, 
-        MediaItemListComponent 
+        MediaItemListComponent,
+        FavoriteDirective
     ], // used to make components, directives and pipes available to your module that dun come from another module
     bootstrap: [ AppComponent ] // used for a root module and let Angular know which component or components will be the starting point for bootstrap process // entry point for app code
 })
