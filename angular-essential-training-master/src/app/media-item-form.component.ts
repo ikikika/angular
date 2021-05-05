c3a827974ba32097b06ff57481d0fc3fed79b6d3
ikikika <iki.jito@gmail.com>
2021-05-05 11:59:26 +0800

    use HttpClient for post, put and delete

--- angular-essential-training-master/src/app/media-item-form.component.ts
+++ angular-essential-training-master/src/app/media-item-form.component.ts
@@ -54,7 +54,7 @@ export class MediaItemFormComponent implements OnInit{
     }
 
     onSubmit(mediaItem){
-        console.log(mediaItem);
-        this.mediaItemService.add(mediaItem);
+        this.mediaItemService.add(mediaItem)
+            .subscribe();
     }
 }
