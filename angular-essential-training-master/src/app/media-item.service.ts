68fb74e8f8a96001be8cdc4a41ccd4f4c71836cd
ikikika <iki.jito@gmail.com>
2021-05-05 13:33:31 +0800

    error handling

--- angular-essential-training-master/src/app/media-item.service.ts
+++ angular-essential-training-master/src/app/media-item.service.ts
@@ -1,33 +1,44 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
-import { map } from 'rxjs/operators'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { map, catchError } from 'rxjs/operators'
+import { throwError } from 'rxjs';
+
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class MediaItemService {
   
-      constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-      get(medium: string) {
-        const getOptions = {
-          params: { medium }
-        }
-        return this.http.get<MediaItemsResponse>('mediaitems', getOptions)
-          .pipe(
-            map((response: MediaItemsResponse) => {
-              return response.mediaItems;
-            })
-          );
-      }
-    
-      add(mediaItem){
-        return this.http.post('mediaItems', mediaItem);
-      }
+  get(medium: string) {
+    const getOptions = {
+      params: { medium }
+    }
+    return this.http.get<MediaItemsResponse>('mediaitems', getOptions)
+      .pipe(
+        map((response: MediaItemsResponse) => {
+          return response.mediaItems;
+        }),
+        catchError(this.handleError)
+      );
+  }
 
-      delete(mediaItem){
-        return this.http.delete(`mediaItems/${mediaItem.id}`);
-      }
+  add(mediaItem){
+    return this.http.post('mediaItems', mediaItem)
+      .pipe(catchError(this.handleError));
+  }
+
+  delete(mediaItem){
+    return this.http.delete(`mediaItems/${mediaItem.id}`)
+      .pipe(catchError(this.handleError));;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error.message);
+    return throwError('data error occured')
+  }
+
 }
 
 export interface MediaItem {
