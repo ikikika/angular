05d6e218ac4d25f0d1055c49b0a0b30c3c4c8eb0
ikikika <iki.jito@gmail.com>
2021-05-05 10:14:21 +0800

    angular httpclient and mock backend for http calls

--- angular-essential-training-master/src/app/app.module.ts
+++ angular-essential-training-master/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'
 import { ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule, HttpXhrBackend } from '@angular/common/http'
 import { AppComponent } from './app.component'
 import { MediaItemComponent } from './media-item.component'
 import { MediaItemListComponent } from './media-item-list.component'
@@ -8,11 +9,13 @@ import { FavoriteDirective } from './favorite.directive'
 import { CategoryListPipe } from './category-list.pipe'
 import { MediaItemFormComponent } from './media-item-form.component';
 import { lookupListToken, lookupLists } from './providers';
+import { MockXHRBackend } from './mock-xhr-backend';
 
 @NgModule({
     imports: [ 
         BrowserModule,
-        ReactiveFormsModule 
+        ReactiveFormsModule,
+        HttpClientModule
     ], // bring in other angular modules that my module will need
     declarations: [ 
         AppComponent, 
@@ -23,9 +26,12 @@ import { lookupListToken, lookupLists } from './providers';
         CategoryListPipe
     ], // used to make components, directives and pipes available to your module that dun come from another module
     providers: [
-        { provide: lookupListToken, useValue: lookupLists}
+        { provide: lookupListToken, useValue: lookupLists},
+        { provide: HttpXhrBackend, useClass: MockXHRBackend}
     ],
-    bootstrap: [ AppComponent ], // used for a root module and let Angular know which component or components will be the starting point for bootstrap process // entry point for app code
+    bootstrap: [ 
+        AppComponent 
+    ], // used for a root module and let Angular know which component or components will be the starting point for bootstrap process // entry point for app code
 })
 export class AppModule {}
 
